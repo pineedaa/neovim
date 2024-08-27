3ff456418f8ddc3ae4f4aa48addbcb9bd784f7d9
Carlos Pineda <crevi2002@gmail.com>
2024-08-27 12:08:10 +0200

    My neovim config

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,35 @@
+local ls_list = { "lua_ls", "bashls", "clangd", "cmake", "cssls", "dockerls", "html", "eslint", "pyright" }
+
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup {
+        ensure_installed = ls_list,
+      }
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require "lspconfig"
+      for _, lang in pairs(ls_list) do
+        lspconfig[lang].setup { capabilities = capabilities }
+      end
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,22 @@
+vim.cmd "set expandtab"
+vim.cmd "set tabstop=2"
+vim.cmd "set softtabstop=2"
+vim.cmd "set shiftwidth=2"
+vim.cmd "set number"
+vim.cmd "set relativenumber"
+
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Disable mouse
+vim.o.mouse = ""
+
+-- Move between splits using Ctrl + h/j/k/l
+vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
+
+-- Copy to system clipboard
+vim.opt.clipboard = "unnamedplus"
