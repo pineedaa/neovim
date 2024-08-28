5c6358c09fc52372b5548a368ff1cb2a46445f6b
Carlos Pineda <crevi2002@gmail.com>
2024-08-28 10:52:58 +0200

    Added a buffer line

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,20 @@
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    vim.opt.termguicolors = true
+    require("bufferline").setup {
+      options = {
+        separator_style = "slant",
+        indicator = {
+          style = "underline",
+        },
+        diagnostics = "nvim_lsp",
+      },
+    }
+
+    vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", {})
+    vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", {})
+  end,
+}
